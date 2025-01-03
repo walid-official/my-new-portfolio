907377098fbcb37f6b132ea65af21392d6bc7920
walid hasan <walid.official8@gmail.com>
2025-01-04 00:03:05 +0600

    my portfolio setup is ready to start

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            hello from navbar
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./router/router.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
